609cf7af6dcfa92c08977f02cb481951f02c8d27
LeonardoGonzalez <leonardo070398@hotmail.com>
2018-10-15 20:22:21 -0300

    Maximo Comun Divisor
    Te da el maximo comun divisor sin terminar.

---
+++ MCD.pas
@@ -0,0 +1,14 @@
+program MCD.pas (input output);
+VAR
+    num1, num2, min,  : Integer;
+
+begin
+
+Writeln('Escribe el pirmer numero ');
+read(num1);
+Writeln('Escribe el 2 numero ');
+read(num2);
+
+
+
+end.
